02eb86a7a3d3fc9cee0b56a385d1967aa3d9bf42
Christa Hartsock <christa.hartsock@gmail.com>
2014-12-31 12:28:25 -0800

    fix truthiness bug

--- twitter/streamtypes.py
+++ twitter/streamtypes.py
@@ -15,12 +15,10 @@ class UserStreamer(StreamListener):
         Tweeta().follow_back(follow)
 
     def on_reply(self, status):
-        print "sender not self:"
-        print status.sender_not_self()
         if status.sender_not_self() and status.is_reply_to_bot():
             text = status.stripped_text()
 
-            if 'in 2015' or 'i fucking did it' in text:
+            if 'in 2015' in text or 'i fucking did it' in text:
                 Tweeta().retweet(status)
 
             # respond appropriately
